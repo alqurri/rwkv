434eb7d85621450b3db8a93624b65a124de2e262
alqurri77 <75499343+alqurri77@users.noreply.github.com>
2025-11-07 15:38:47 -0500

    Update train.py

--- train.py
+++ train.py
@@ -23,6 +23,7 @@ parser.add_argument('--max_iterations', type=int,
 parser.add_argument('--max_epochs', type=int,
                     default=150, help='maximum epoch number to train')
 parser.add_argument('--batch_size', type=int,
+                    default=8, help='batch_size per gpu')# use bigger batch size if you have GPU memeory
 parser.add_argument('--n_gpu', type=int, default=1, help='total gpu')
 parser.add_argument('--deterministic', type=int, default=1,
                     help='whether use deterministic training')
